d637f433e490cf246b2cd9ea7279fb5e4ef97ad4
Hector Garcia <hector032716@gmail.com>
2023-02-17 20:22:37 -0800

    ⭐ Add base card

---
+++ src/components/FeaturedCard/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function FeaturedCard({ project }) {
+  return (
+    <div className='flex flex-col sm:flex-row h-[30rem] sm:h-52 gap-5'>
+      <div className='rounded-lg overflow-hidden sm:flex-[1_1_0%]'>
+        <img
+          className='w-full h-full object-cover'
+          src={project.image}
+          alt={project.title}/>
+      </div>
+
+      <div className='flex flex-col gap-4 sm:flex-[2_2_0%]'>
+        <h3 className='text-3xl font-semibold text-dark'>{project.title}</h3>
+        <div className='flex items-center gap-5'>
+          <span className='px-3 text-white bg-dark font-bold rounded-full'>{project.year}</span>
+          <span className='text-gray-400 text-lg'>{project.type}</span>
+        </div>
+        <p>{project.description}</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default FeaturedCard
---
+++ src/components/FeaturedCard/index.stories.jsx
@@ -0,0 +1,20 @@
+import FeaturedCard from './index'
+
+export default {
+  component: FeaturedCard,
+  title: 'FeaturedCard',
+  decorators: [story => <div style={{ maxWidth: '800px', margin: 'auto' }}>{story()}</div>],
+}
+
+
+const Template = (args) => <FeaturedCard {...args} />
+export const Default = Template.bind({})
+Default.args = {
+  project: {
+    image: 'https://picsum.photos/200/300',
+    title: 'Project title',
+    year: 2022,
+    type: 'Dashboard',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nisl. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nisl.',
+  }
+}
