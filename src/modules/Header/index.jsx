f82acb5a4d7cb228dd7eab2382bfbd7decd01bb6
Arturo Mendivil <arturoo1516@gmail.com>
2023-02-25 23:24:34 -0800

    style header and add it to layout

--- src/modules/Header/index.jsx
+++ src/modules/Header/index.jsx
@@ -2,23 +2,16 @@ import React, { ReactNode } from 'react';
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   Link,
   IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-const Links = ['Dashboard', 'Projects', 'Team'];
+const Links = ['Projects', 'Blog', 'Contact'];
 
 const NavLink = ({ children }) => (
   <Link
@@ -27,7 +20,7 @@ const NavLink = ({ children }) => (
     rounded={'md'}
     _hover={{
       textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      color: 'tomato',
     }}
     href={'#'}>
     {children}
@@ -39,8 +32,8 @@ export default function Simple() {
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      <Box bg={useColorModeValue('white', 'white')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'right'}>
           <IconButton
             size={'md'}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -49,7 +42,7 @@ export default function Simple() {
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
+            {/* <Box>Logo</Box> */}
             <HStack
               as={'nav'}
               spacing={4}
@@ -59,29 +52,6 @@ export default function Simple() {
               ))}
             </HStack>
           </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
         </Flex>
 
         {isOpen ? (
@@ -94,8 +64,6 @@ export default function Simple() {
           </Box>
         ) : null}
       </Box>
-
-      <Box p={4}>Main Content Here</Box>
     </>
   );
 }
--- src/modules/Layout/index.js
+++ src/modules/Layout/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from '../Footer'
+import Header from '../Header'
 
-function Layout({children}) {
+function Layout({ children }) {
   return (
     <div>
-      Header
+      <Header />
       {children}
-      <Footer text='Copyright ©2023 All rights reserved'/>
+      <Footer text='Copyright ©2023 All rights reserved' />
     </div>
   )
 }
