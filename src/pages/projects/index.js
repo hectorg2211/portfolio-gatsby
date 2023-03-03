d04dfd933069c8ac1d166e0589fd26e2d5f31e14
Hector Garcia <hector032716@gmail.com>
2023-03-02 20:15:27 -0800

    ⭐ Add trigger

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -8,7 +8,6 @@ function Projects({data}) {
     <Layout>
       <main className='container m-auto xl:px-32 flex flex-col gap-10'>
         <h1 className='text-3xl sm:text-5xl font-semibold text-dark'>Projects</h1>
-
         {data.allSanityProject.projects.map((project,index) =>
           <div key={index} className='pb-4 border-b-gray-300 border-b'>
             <FeaturedCard project={project}/>
